8c14eb895aa3073d8d878913cb11469e5e0f224f
KassandraSaint <kassandrastripper@gmail.com>
2022-06-20 13:27:34 -0400

    Add project files.

---
+++ MorseCodeConverter/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace MorseCodeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.stringBox = new System.Windows.Forms.TextBox();
+            this.codeBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.codeBox = new System.Windows.Forms.TextBox();
+            this.stringBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.morseBox = new System.Windows.Forms.TextBox();
+            this.convertedBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(702, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a String you want to convert to Morse Code:";
+            // 
+            // stringBox
+            // 
+            this.stringBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringBox.Location = new System.Drawing.Point(33, 86);
+            this.stringBox.Name = "stringBox";
+            this.stringBox.Size = new System.Drawing.Size(730, 43);
+            this.stringBox.TabIndex = 1;
+            // 
+            // codeBtn
+            // 
+            this.codeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.codeBtn.Font = new System.Drawing.Font("Microsoft YaHei", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeBtn.Location = new System.Drawing.Point(265, 155);
+            this.codeBtn.Name = "codeBtn";
+            this.codeBtn.Size = new System.Drawing.Size(245, 120);
+            this.codeBtn.TabIndex = 2;
+            this.codeBtn.Text = "Convert\r\nto\r\nMorse Code";
+            this.codeBtn.UseVisualStyleBackColor = false;
+            this.codeBtn.Click += new System.EventHandler(this.codeBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(702, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Morse Code you want to convert to a String:";
+            // 
+            // codeBox
+            // 
+            this.codeBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeBox.Location = new System.Drawing.Point(33, 528);
+            this.codeBox.Name = "codeBox";
+            this.codeBox.Size = new System.Drawing.Size(730, 43);
+            this.codeBox.TabIndex = 5;
+            // 
+            // stringBtn
+            // 
+            this.stringBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.stringBtn.Font = new System.Drawing.Font("Microsoft YaHei", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringBtn.Location = new System.Drawing.Point(265, 602);
+            this.stringBtn.Name = "stringBtn";
+            this.stringBtn.Size = new System.Drawing.Size(245, 120);
+            this.stringBtn.TabIndex = 6;
+            this.stringBtn.Text = "Convert\r\nto\r\nString";
+            this.stringBtn.UseVisualStyleBackColor = false;
+            this.stringBtn.Click += new System.EventHandler(this.stringBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft YaHei", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.ForeColor = System.Drawing.Color.Black;
+            this.exitBtn.Location = new System.Drawing.Point(287, 916);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(210, 70);
+            this.exitBtn.TabIndex = 8;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // morseBox
+            // 
+            this.morseBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.morseBox.Location = new System.Drawing.Point(33, 306);
+            this.morseBox.Multiline = true;
+            this.morseBox.Name = "morseBox";
+            this.morseBox.ReadOnly = true;
+            this.morseBox.Size = new System.Drawing.Size(730, 145);
+            this.morseBox.TabIndex = 3;
+            this.morseBox.TabStop = false;
+            // 
+            // convertedBox
+            // 
+            this.convertedBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertedBox.Location = new System.Drawing.Point(33, 748);
+            this.convertedBox.Multiline = true;
+            this.convertedBox.Name = "convertedBox";
+            this.convertedBox.ReadOnly = true;
+            this.convertedBox.Size = new System.Drawing.Size(730, 145);
+            this.convertedBox.TabIndex = 7;
+            this.convertedBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.codeBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.exitBtn;
+            this.ClientSize = new System.Drawing.Size(794, 998);
+            this.Controls.Add(this.convertedBox);
+            this.Controls.Add(this.morseBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.stringBtn);
+            this.Controls.Add(this.codeBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.codeBtn);
+            this.Controls.Add(this.stringBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Morse Code Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox stringBox;
+        private System.Windows.Forms.Button codeBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox codeBox;
+        private System.Windows.Forms.Button stringBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox morseBox;
+        private System.Windows.Forms.TextBox convertedBox;
+    }
+}
+
---
+++ MorseCodeConverter/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorseCodeConverter
+{
+    public partial class Form1 : Form
+    {
+        // Initializing constant variable to hold size of the arrays
+        const int SIZE = 50;
+        // Creating an array of 50 characters
+        char[] characters = new char[SIZE] 
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '.', ',', ':', '"', '\'', '!', '?', '@', '-', ';', '(', ')', '=',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' '
+        };
+        // Creating an array of 50 morse code symbols
+        string[] mCode = new string[SIZE]
+        {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+            "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+            ".-.-.-", "--..--", "---...", ".-..-.", ".----.", "-.-.--", "..--..", ".--.-.", "-....-", "-.-.-.", "-.--.", "-.--.-", "-...-",
+            ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Creating a method to convert string to a mrse code
+        private string stringyMorse(string stringy)
+        {
+            // Initializing a string variable to hold resulting string of morse code
+            string morseString = "";
+
+            // Traversing over every character of the string and looking for a index number in the array of chars
+            for (int i = 0; i < stringy.Length; i++)
+            {
+                // Looking for a position of the character from the string in the array of charachters
+                int position = Array.IndexOf(characters, stringy[i]);
+
+                // if position is out of range, aka if charachter is not in the array
+                if (position < 0)
+                {
+                    // Alerting a user that string entered contains unsupported character
+                    MessageBox.Show("String you entered contains unsupported character that can't be converted to Morse Code");
+                    morseString = "";
+                    break;
+                }
+                else
+                {
+                    // Constracting a Morse Code string
+                    morseString += mCode[position] + " ";
+                }
+            }
+
+            // Returning string of morse code
+            return morseString;
+        }
+
+        // Creating a method to convert morse code to a string
+        private string morsyString(string code)
+        {
+            // Initializing a string variable strin to hold converted morse code to string
+            string strin = "";
+
+            // Tokenizing elements from morse code string into array 
+            string[] tokens = code.Split(null);
+
+            // Going over each token of tokens array and determining index of that element in the mCode array
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                int position = Array.IndexOf(mCode, tokens[i]);
+
+                // if position is out of range, aka if morse code is not in the array
+                if (position < 0)
+                {
+                    // Alerting a user that string entered contains unsupported character
+                    MessageBox.Show("String you entered contains unsupported character that can't be converted from Morse Code");
+                    strin = "";
+                    break;
+                }
+                else
+                {
+                    // Constructing a string from morse code
+                    strin += characters[position];
+                }
+            }
+
+            // Returning string
+            return strin;
+        }
+
+        private void codeBtn_Click(object sender, EventArgs e)
+        {
+            // Getting a string from the user 
+            string stringy = stringBox.Text;
+            // Taking care of the case for letters
+            string stringyLow = stringy.ToLower();
+
+            // Calling a stringyMorse converter method and passing a lower cased string as an argument
+            // Storing returned string in the morse variable
+            string morse = stringyMorse(stringyLow);
+
+            // Displaying morse code
+            morseBox.Text = morse;
+        }
+
+        private void stringBtn_Click(object sender, EventArgs e)
+        {
+            // Getting amorse code from the user
+            string code = codeBox.Text;
+            // Trimming any white spaces
+            string codeTrimmed = code.Trim();
+
+            // Calling a morsyString method to convert morse code to a string
+            // Storing returned string in the converted variable
+            string converted = morsyString(codeTrimmed);
+
+            // Displaying the string converted from morse code
+            // Convert case to upper as on https://morsecode.world
+            convertedBox.Text = converted.ToUpper();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // Closing the form
+            this.Close();
+        }
+    }
+}
